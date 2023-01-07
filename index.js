df268474b638c2c358824a71d98f3b149f529610
Melissa Modesto <mbm.modesto@gmail.com>
2023-01-07 14:04:58 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from  'dotenv';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+}
+);
